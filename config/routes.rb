1b2db51da9c4d2fa942ae362adf0de30b664971e
Jason Kadman <jkadman231@gmail.com>
2023-04-17 08:15:53 +0000

    create category routes with namespace

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  namespace :admin do
+    resources :categories, only: [:index, :create, :new]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
